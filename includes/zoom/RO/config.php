9d6544ef898cf66479a87cfb81624d62a5945f9e
Phage <dace.phage@gmail.com>
2024-06-26 16:07:44 +0800

    zoom RO

--- includes/zoom/RO/config.php
+++ includes/zoom/RO/config.php
@@ -4,4 +4,4 @@ require_once "db.php";
  
 define('CLIENT_ID', 'VITDKqD2SVCCxsl6ceP0Q');
 define('CLIENT_SECRET', 'm4LVXH2awDRTQyUextRhtORr6K8Hj3dJ');
-define('REDIRECT_URI', 'http://r6itbpm.site/isds/includes/zoom/Aklan/callback.php');
+define('REDIRECT_URI', 'http://r6itbpm.site/isds/includes/zoom/RO/callback.php');
