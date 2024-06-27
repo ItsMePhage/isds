0333a011eaf7cc2c2da4d05602fb79411a3bc474
Phage <dace.phage@gmail.com>
2024-06-27 11:20:35 +0800

    zoom update

--- includes/zoom/RO/create-meeting.php
+++ includes/zoom/RO/create-meeting.php
@@ -1,13 +1,14 @@
 <?php
 require_once 'config.php';
- 
-function create_meeting() {
+
+function create_meeting($topic, $start_time, $duration)
+{
     $client = new GuzzleHttp\Client(['base_uri' => 'https://api.zoom.us']);
- 
+
     $db = new DB();
     $arr_token = $db->get_access_token();
     $accessToken = $arr_token->access_token;
- 
+
     try {
         // if you have userid of user than change it with me in url
         $response = $client->request('POST', '/v2/users/me/meetings', [
@@ -15,27 +16,26 @@ function create_meeting() {
                 "Authorization" => "Bearer $accessToken"
             ],
             'json' => [
-                "topic" => "Integrate zoom APIs",
-                "type" => 2,                              
-                "start_time" => "2020-06-24T20:30:00",    // meeting start time
-                "duration" => "30",                       // 30 minutes
-                "password" => "123456"                    // meeting password
+                "topic" => "$topic",
+                "type" => 2,
+                "start_time" => "$start_time",
+                "duration" => "$duration"
             ],
         ]);
- 
+
         $data = json_decode($response->getBody());
-        echo "Join URL: ". $data->join_url;
+        echo "Join URL: " . $data->join_url;
         echo "<br>";
-        echo "Meeting Password: ". $data->password;
- 
-    } catch(Exception $e) {
-        if( 401 == $e->getCode() ) {
+        echo "Meeting Password: " . $data->password;
+
+    } catch (Exception $e) {
+        if (401 == $e->getCode()) {
             $refresh_token = $db->get_refersh_token();
- 
+
             $client = new GuzzleHttp\Client(['base_uri' => 'https://zoom.us']);
             $response = $client->request('POST', '/oauth/token', [
                 "headers" => [
-                    "Authorization" => "Basic ". base64_encode(CLIENT_ID.':'.CLIENT_SECRET)
+                    "Authorization" => "Basic " . base64_encode(CLIENT_ID . ':' . CLIENT_SECRET)
                 ],
                 'form_params' => [
                     "grant_type" => "refresh_token",
@@ -43,12 +43,12 @@ function create_meeting() {
                 ],
             ]);
             $db->update_access_token($response->getBody());
- 
+
             create_meeting();
         } else {
             echo $e->getMessage();
         }
     }
 }
- 
-create_meeting();
+
+create_meeting("Sample meeting", "2024-06-28T20:30:00", "150");
