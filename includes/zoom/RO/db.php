401cfdb2d5d2cafec43fc9bcebe36fc24f7d7902
Phage <dace.phage@gmail.com>
2024-06-28 16:05:29 +0800

    RO

--- includes/zoom/RO/db.php
+++ includes/zoom/RO/db.php
@@ -1,53 +1,63 @@
 <?php
-class DB {
+class DB
+{
     // Database credentials
-    private $dbHost     = "localhost";
+    private $dbHost = "localhost";
     private $dbUsername = "zoomrequestadmin";
     private $dbPassword = "!r7TG4WuxCRJUgoo";
-    private $dbName     = "zoom_tokens";
- 
-    public function __construct(){
-        if(!isset($this->db)){
+    private $dbName = "zoom_tokens";
+
+    public function __construct()
+    {
+        if (!isset($this->db)) {
             // Connect to the database
             $conn = new mysqli($this->dbHost, $this->dbUsername, $this->dbPassword, $this->dbName);
-            if($conn->connect_error){
+            if ($conn->connect_error) {
                 die("Failed to connect with MySQL: " . $conn->connect_error);
-            }else{
+            } else {
                 $this->db = $conn;
             }
         }
     }
-  
+
     // Check is table empty
-    public function is_table_empty() {
+    public function is_table_empty()
+    {
         $result = $this->db->query("SELECT id FROM `RO`");
-        if($result->num_rows) {
+        if ($result->num_rows) {
             return false;
         }
-  
+
         return true;
     }
-  
+
     // Get access token
-    public function get_access_token() {
+    public function get_access_token()
+    {
         $sql = $this->db->query("SELECT access_token FROM `RO`");
         $result = $sql->fetch_assoc();
         return json_decode($result['access_token']);
     }
-  
+
     // Get referesh token
-    public function get_refersh_token() {
+    public function get_refersh_token()
+    {
         $result = $this->get_access_token();
         return $result->refresh_token;
     }
-  
+
     // Update access token
-    public function update_access_token($token) {
-        if($this->is_table_empty()) {
+    public function update_access_token($token)
+    {
+        if ($this->is_table_empty()) {
             $this->db->query("INSERT INTO `RO`(access_token) VALUES('$token')");
         } else {
-            $this->db->query("UPDATE `RO` SET access_token = '$token' WHERE id = (SELECT id FROM `RO`)");
-            echo $this->db->error;
+            $sql = "UPDATE `RO` SET access_token = '$token' WHERE id = (SELECT id FROM `RO`)";
+            if ($this->db->query($sql) === TRUE) {
+                echo "Record updated successfully";
+            } else {
+                echo "Error updating record: " . $this->db->error;
+            }
         }
     }
 }
