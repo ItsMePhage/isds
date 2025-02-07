7c51add3c316e7ce0678cf9347382ff1a46ee48a
phage <danalfreifuerte@gmail.com>
2025-02-07 11:01:44 +0800

    070225

--- assets/js/main.js
+++ assets/js/main.js
@@ -1353,7 +1353,7 @@ $(function () {
           dataLabels: {
             enabled: true,
             formatter: function (val, opt) {
-              console.log(opt);
+              console.log(opt.dataPointIndex);
               return opt.w.globals.labels[opt.dataPointIndex] + ": " + val.toFixed(2) + "%";
             },
           },
